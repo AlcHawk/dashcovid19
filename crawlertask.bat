ead034fad1617bf17bcc982bd51dbdaccb417158
alchawk <hawkerylin@gmail.com>
2020-08-26 15:43:02 +0800

    add scheduled crawler task batch

---
+++ crawlertask.bat
@@ -0,0 +1,18 @@
+set /p dashdir=<dashdirectoryname.txt
+
+C:
+cd /
+cd %dashdir%
+call conda activate dashcovid19
+REM python download_source.py
+call conda deactivate
+
+set tdy=%date:~0, 4%%date:~5, 2%%date:~8, 2%
+call echo "Today is %tdy%"
+git add Data/"CDC_Press List.xlsx"
+git add Data/"COVID-19_TW.xlsx"
+git commit -m "Update CDC Press and COVID-19 Statistic on %tdy%"
+git push -u origin master
+
+REM call echo %dashdir%
+REM call echo '' > demo.txt
